054223a4e0cd2a1fa02a22dbc68fd6f064607415
domjgiordano <dominick.giordano@payzer.com>
2022-09-15 16:37:28 -0400

    Login Page complete - functionality for spotify api added

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 8888;
+const application = express();
+
+application.use(express.json());
+application.use(express.urlencoded({ extended: true}));
+application.use(cors());
+
+const AuthRoutes = require('./routes/authRoutes.js');
+application.use('/api', cors(), AuthRoutes);
+
+application.get('', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+application.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
