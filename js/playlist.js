28615449e1349edc6307a749d0ed8bb6ab40b5fd
domjgiordano <dominick.giordano@payzer.com>
2022-09-19 20:30:12 -0400

    Added feature sliders - working

--- js/playlist.js
+++ js/playlist.js
@@ -31,27 +31,41 @@ export const renderPage = function() {
     </div>
 
     <div class="hero-body">
+
       <div id="main" class="container has-text-centered">
-        <p class="title is-1">
-            Select Your Filters
-        </p>
-        <br>
-        <p class="subtitle is-6" style="text-align: center"> Dancability Value: <span id="danceVal"></span></p>
-        <input id="danceSlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value=".50" type="range">
-
-        <br>
-        <p class="subtitle is-6" style="text-align: center"> Loudness Value: <span id="loudVal"></span></p>
-        <input id="loudSlider" class="slider has-output-tooltip is-fullwidth" step="1" min="-60" max="0" value="-30" type="range">
-
-        <br>
-        <p class="subtitle is-6" style="text-align: center"> Tempo Value: <span id="tempoVal"></span></p>
-        <input id="tempoSlider" class="slider has-output-tooltip is-fullwidth" step="10" min="0" max="200" value="100" type="range">
-
-        <br>
-        <p class="subtitle is-6" style="text-align: center"> Energy Value: <span id="energyVal"></span></p>
-        <input id="energySlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value=".50" type="range">
-
-        <br>
+        <p class="title is-1">Select Filters</p>
+        <div class="columns is-multiline">
+            <div class="column is-one-quarter">
+                <p class="subtitle is-6" style="text-align: center"> Dancability Value: <span id="danceVal"> 0 </span> to <span id="danceValMax"> 1.00 </span></p>
+                MIN: <input id="danceSlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="0" type="range"><br>
+                MAX: <input id="danceSliderMax" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="1.00" type="range">
+            </div>
+            <div class="column is-one-quarter">
+                <p class="subtitle is-6" style="text-align: center"> Loudness Value: <span id="loudVal"> -60 </span> to <span id="loudValMax"> 0.00 </span></p>
+                MIN: <input id="loudSlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="-60.00" max="0.00" value="-60.00" type="range"><br>
+                MAX: <input id="loudSliderMax" class="slider has-output-tooltip is-fullwidth" step=".01" min="-60.00" max="0.00" value="0.00" type="range">
+            </div>
+            <div class="column is-one-quarter">
+                <p class="subtitle is-6" style="text-align: center"> Tempo Value: <span id="tempoVal"> 0 </span> to <span id="tempoValMax"> 200 </span></p>
+                MIN: <input id="tempoSlider" class="slider has-output-tooltip is-fullwidth" step="1" min="0" max="200" value="0" type="range"><br>
+                MAX: <input id="tempoSliderMax" class="slider has-output-tooltip is-fullwidth" step="1" min="0" max="200" value="200" type="range">
+            </div>
+            <div class="column is-one-quarter">
+                <p class="subtitle is-6" style="text-align: center"> Energy Value: <span id="energyVal"> 0 </span> to <span id="energyValMax"> 1.00 </span></p>
+                MIN <input id="energySlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="0" type="range"><br>
+                MAX <input id="energySliderMax" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="1.00" type="range">
+            </div>
+            <div class="column is-one-quarter">
+                <p class="subtitle is-6" style="text-align: center"> Speechiness Value: <span id="speechVal"> 0 </span> to <span id="speechValMax"> 1.00 </span></p>
+                MIN: <input id="speechSlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="0" type="range"><br>
+                MAX: <input id="speechSliderMax" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="1.00" type="range">
+            </div>
+            <div class="column is-one-quarter">
+                <p class="subtitle is-6" style="text-align: center"> Valence Value: <span id="valenceVal"> 0 </span> to <span id="valenceValMax"> 1.00 </span></p>
+                MIN: <input id="valenceSlider" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="0" type="range"><br>
+                MAX: <input id="valenceSliderMax" class="slider has-output-tooltip is-fullwidth" step=".01" min="0.00" max="1.00" value="1.00" type="range">
+            </div>
+        </div>
         <button id="playlistBtn" class="button is-link is-light is-large is-outlined is-rounded">
           SUBMIT
         </button>
@@ -73,7 +87,34 @@ export const renderPage = function() {
     return;
   }
 
-  async function getSongs(danceVal=-50, tempoVal=-50, loudVal=-50, energyVal=-50) {
+  async function getSongs(filterVals) {
+
+    //Check filter values
+    if(filterVals['energyMin'] >= filterVals['energyMax']){
+        alert("Invalid Energy Values. Try again.")
+        return;
+    }
+    if(filterVals['danceMin'] >= filterVals['danceMax']){
+        alert("Invalid Dancability Values. Try again.")
+        return;
+    }
+    if(filterVals['tempoMin'] >= filterVals['tempoMax']){
+        alert("Invalid Tempo Values. Try again.")
+        return;
+    }
+    if(filterVals['loudMin'] >= filterVals['loudMax']){
+        alert("Invalid Loudness Values. Try again.")
+        return;
+    }
+    if(filterVals['speechMin'] >= filterVals['speechMax']){
+        alert("Invalid Speechiness Values. Try again.")
+        return;
+    }
+    if(filterVals['valenceMin'] >= filterVals['valenceMax']){
+        alert("Invalid Valence Values. Try again.")
+        return;
+    }
+
     const maxSongs = 50;
     let playlistSongCount=0;
     let songInfo = ''
@@ -108,7 +149,13 @@ export const renderPage = function() {
         const songData = await songResponse.json();
 
 
-        if (songData.energy > energyVal && songData.danceability > danceVal && songData.tempo > tempoVal && songData.loudness > loudVal){
+        if (songData.energy > filterVals['energyMin'] && songData.energy < filterVals['energyMax'] &&
+                songData.danceability > filterVals['danceMin'] && songData.danceability < filterVals['danceMax'] &&
+                songData.tempo > filterVals['tempoMin'] && songData.tempo < filterVals['tempoMax'] &&
+                songData.loudness > filterVals['loudMin'] && songData.loudness < filterVals['loudMax'] &&
+                songData.speechiness > filterVals['speechMin'] && songData.speechiness < filterVals['speechMax'] &&
+                songData.valence > filterVals['valenceMin'] && songData.valence < filterVals['valenceMax']){
+
             playlistSongCount++;
             songInfo+='<div class="column is-one-quarter" style="margin: 1%; width: 31%">';
             songInfo+='<div class="card-content" style="text-align: center">';
@@ -122,6 +169,8 @@ export const renderPage = function() {
             songInfo+='<p class="subtitle is-6" style="text-align: center"> DANCEABILITY: ' + songData.danceability + '</p>';
             songInfo+='<p class="subtitle is-6" style="text-align: center"> TEMPO: ' + songData.tempo + '</p>';
             songInfo+='<p class="subtitle is-6" style="text-align: center"> LOUDNESS: ' + songData.loudness + '</p>';
+            songInfo+='<p class="subtitle is-6" style="text-align: center"> TEMPO: ' + songData.speechiness + '</p>';
+            songInfo+='<p class="subtitle is-6" style="text-align: center"> LOUDNESS: ' + songData.valence + '</p>';
             songInfo+='</div></div></div></div>';
         }
         songIds[i-1] = data.items[i-1].track.id
@@ -154,16 +203,40 @@ export const renderPage = function() {
     var tempoSlider = document.getElementById("tempoSlider");
     var loudSlider = document.getElementById("loudSlider");
     var energySlider = document.getElementById("energySlider");
+    var speechSlider = document.getElementById("speechSlider");
+    var valenceSlider = document.getElementById("valenceSlider");
+    var danceSliderMax = document.getElementById("danceSliderMax");
+    var tempoSliderMax = document.getElementById("tempoSliderMax");
+    var loudSliderMax = document.getElementById("loudSliderMax");
+    var energySliderMax = document.getElementById("energySliderMax");
+    var speechSliderMax = document.getElementById("speechSliderMax");
+    var valenceSliderMax = document.getElementById("valenceSliderMax");
 
     var danceVal = document.getElementById("danceVal");
     var tempoVal = document.getElementById("tempoVal");
     var loudVal = document.getElementById("loudVal");
     var energyVal = document.getElementById("energyVal");
+    var speechVal = document.getElementById("speechVal");
+    var valenceVal = document.getElementById("valenceVal");
+    var danceValMax = document.getElementById("danceValMax");
+    var tempoValMax = document.getElementById("tempoValMax");
+    var loudValMax = document.getElementById("loudValMax");
+    var energyValMax = document.getElementById("energyValMax");
+    var speechValMax = document.getElementById("speechValMax");
+    var valenceValMax = document.getElementById("valenceValMax");
 
     danceVal.innterHTML = danceSlider.nodeValue;
     tempoVal.innterHTML = tempoSlider.nodeValue;
     loudVal.innterHTML = loudSlider.nodeValue;
     energyVal.innerHTML = energySlider.nodeValue;
+    speechVal.innterHTML = speechSlider.nodeValue;
+    valenceVal.innerHTML = valenceSlider.nodeValue;
+    danceValMax.innterHTML = danceSliderMax.nodeValue;
+    tempoValMax.innterHTML = tempoSliderMax.nodeValue;
+    loudValMax.innterHTML = loudSliderMax.nodeValue;
+    energyValMax.innerHTML = energySliderMax.nodeValue;
+    speechValMax.innterHTML = speechSliderMax.nodeValue;
+    valenceValMax.innerHTML = valenceSliderMax.nodeValue;
 
     danceSlider.oninput = function() {
         danceVal.innerHTML = this.value;
@@ -177,10 +250,48 @@ export const renderPage = function() {
     energySlider.oninput = function() {
         energyVal.innerHTML = this.value;
     }
+    speechSlider.oninput = function() {
+        speechVal.innerHTML = this.value;
+    }
+    valenceSlider.oninput = function() {
+        valenceVal.innerHTML = this.value;
+    }
+    danceSliderMax.oninput = function() {
+        danceValMax.innerHTML = this.value;
+    }
+    tempoSliderMax.oninput = function() {
+        tempoValMax.innerHTML = this.value;
+    }
+    loudSliderMax.oninput = function() {
+        loudValMax.innerHTML = this.value;
+    }
+    energySliderMax.oninput = function() {
+        energyValMax.innerHTML = this.value;
+    }
+    speechSliderMax.oninput = function() {
+        speechValMax.innerHTML = this.value;
+    }
+    valenceSliderMax.oninput = function() {
+        valenceValMax.innerHTML = this.value;
+    }
 
     $(document).ready(function() {
         $("#playlistBtn").click(function(){
-            getSongs(danceSlider.value, tempoSlider.value, loudSlider.value, energySlider.value);
+            var filterVals = {
+                danceMin: danceSlider.value,
+                danceMax: danceSliderMax.value,
+                tempoMin: tempoSlider.value,
+                tempoMax: tempoSliderMax.value,
+                loudMin: loudSlider.value,
+                loudMax: loudSliderMax.value,
+                energyMin: energySlider.value,
+                energyMax: energySliderMax.value,
+                speechMin: speechSlider.value,
+                speechMax: speechSliderMax.value,
+                valenceMin: valenceSlider.value,
+                valenceMax: valenceSliderMax.value,
+            };
+            getSongs(filterVals);
         });
     });
 
